f211e136abcba7d53697e998f4c7e9ebf6a53d24
Aditya Patil <aditya29890patil@gmail.com>
2026-06-11 15:37:52 +0530

    Start learning javascript and complte assignment.

---
+++ JavaScript/1JS/script.js
@@ -0,0 +1,53 @@
+console.log("Aditya is great by external JS.");
+
+let num_1 = 4;
+let num_2 = 5;
+console.log(num_1 + num_2);
+let sum = num_1 + num_2;
+console.log(sum);
+
+
+// LET Keyword Example
+let Father = "Son";
+console.log(Father);
+
+Father = "Daughter";
+console.log(Father);
+
+Father = 45;
+console.log(Father + " years old.");
+console.log(`Father is ${Father} years old.`)
+
+
+// CONST Keyword Example
+const Mother = "Daughter";
+console.log(Mother);
+
+// Mother = "Son";                  // Gives Error
+// console.log(Mother);
+
+var c = 30;
+let a = 10;
+console.log(a);
+
+const d = 90;
+console.log(d);
+{
+    let b = 20;
+
+    console.log(b);
+    console.log(a);
+    console.log(c); 
+
+    var d = 40;
+
+    const d = 90;
+    console.log(d);
+}
+console.log(c);
+console.log(d);
+// console.log(b);
+
+// const d = 90;            // Give Error
+// console.log(d);
+
