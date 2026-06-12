f534df873c6579442f0797c909989b176616953f
Aditya Patil <aditya29890patil@gmail.com>
2026-06-12 12:37:08 +0530

    Learn about Undefined, Null and TypeCasting.

---
+++ JavaScript/1JS/variables.js
@@ -0,0 +1,21 @@
+// Undefined - Declared but not Initialized
+let a;
+console.log(a);
+console.log(typeof(a));
+
+// Null - Declared and Initialized with null
+let b = null;
+console.log(b);
+console.log(typeof(b));
+
+// typeOf() method - Used to check Data Type
+let c = "7";
+let d = 7;
+console.log(typeof(c));
+console.log(typeof(d));
+
+// TypeCasting
+let z = Number(c);
+let y = String(d);
+console.log(typeof(z));
+console.log(typeof(y));
